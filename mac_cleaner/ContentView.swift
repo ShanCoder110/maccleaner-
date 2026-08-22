d167723b2450b9059387674c02e7e70c266b4ca4
Shan Abbas <shankhanbkr@gmail.com>
2026-08-22 15:38:18 +0500

    Initial Commit

---
+++ mac_cleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mac_cleaner
+//
+//  Created by m2pro on 22/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mac_cleaner/mac_cleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_cleanerApp.swift
+//  mac_cleaner
+//
+//  Created by m2pro on 22/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct mac_cleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
